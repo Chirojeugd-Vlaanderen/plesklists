2e2ddacb5fca26718408f99dd1380a532b4911ec
Johan Vervloet <johanv@johanv.org>
2015-05-19 09:37:28 +0200

    Closes #7 - Plesklists.get API method.

---
+++ api/v3/Plesklists/Get.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Plesklists.Get API.
+ *
+ * For the moment this just returns all plesk lists with their
+ * CiviCRM group IDs.
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_plesklists_Get($params) {
+  $all_lists = CRM_Plesklists_Helper::getLists();
+  $returnValues = array();
+
+  foreach ($all_lists as $group_id => $list_name) {
+    $returnValues[$group_id] = array (
+      'id' => $group_id,
+      'plesk_list' => $list_name,
+    );
+  }
+
+  return civicrm_api3_create_success($returnValues, $params, 'Plesklists', 'get');
+}
+
